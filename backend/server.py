772fb936572aeaaf5adc0b6d8ff7f09cb3f49016
harik8 <khprasaad@gmail.com>
2022-01-09 12:57:08 +0100

    Update server module to use Mongo DATA API

--- backend/server.py
+++ backend/server.py
@@ -1,33 +1,45 @@
-import pymongo
-from bottle import route, request, response, run 
-import bottle
-import logging
+import bottle 
 import json
-import os
+import logging
+import os, sys
+import requests
 
-def db_connect():
-    client = pymongo.MongoClient(os.environ['MONGO_END_POINT'])
-    return client
+from bottle import route, request, response, run
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+MONGO_API_KEY = os.environ['MONGO_DATA_API_KEY']
+MONGO_DATA_END_POINT = os.environ['MONGO_DATA_END_POINT']
+
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'api-key': "{0}".format(MONGO_API_KEY)
+}
     
 app = bottle.app()
 
 @app.route('/adddiscount', method=['POST','OPTIONS'])
 def add_discount():
-    db = db_connect()['dportal']['discounts']
-    
     response.headers['Content-type'] = 'application/json'
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
 
-    results = None
-    if bottle.request.method != 'OPTIONS':
-        try:
-            results = db.insert_one(request.json['obj']).inserted_id
-            response.body(results)
-            logging.info("Results - {}".format(results))
-        except Exception as e:
-            logging.error(e)
+    try:
+        if bottle.request.method != 'OPTIONS':
+            payload = json.dumps({
+                "collection": "discounts",
+                "database": "dportal",
+                "dataSource": "discount-portal",
+                "document": request.json['obj']
+            })
+            results = requests.request("POST", MONGO_DATA_END_POINT, headers=headers, data=payload)
 
-    return response
+            logging.info("RESPONSE - {0}".format(results))
+            return response
+    except Exception as e:
+        logging.error(e)
+        return e
 
 app.run(host='127.0.0.1',port=5000, debug=True)
